21f29d99b96a78f8363c8f6748c782fb239b5cb2
Baudlicious <x3830@protonmail.com>
2019-09-09 10:37:53 -0400

    Started Setup Script

---
+++ setup.ps1
@@ -0,0 +1,50 @@
+#------------------------------------------------------------------------------
+# > Environment
+#------------------------------------------------------------------------------
+$github = "C:\Github"
+$tools = "C:\Tools"
+
+# TODO: Create a C:\Tools folder
+# TODO: Install Scoop
+Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
+# TODO: Install aria2
+scoop install aria2
+# TODO: Install FireFox
+scoop install Firefox
+# TODO: Create a C:\Github folder
+# TODO: Scoop bucket add extras
+scoop bucket add extras
+
+#------------------------------------------------------------------------------
+# > Utilities
+#------------------------------------------------------------------------------
+# 100% Always utilities
+# TODO: Install 7zip
+# TODO: Install NerdFonts
+# TODO: Install Notepadplusplus
+# TODO: Install NVIM
+### TODO: Setup NVIM with Windows-Environment 
+# TODO: Install Git
+
+# Bonus Utilities
+# TODO: Install WireShark
+# TODO: Install Wxhexeditor
+# TODO: Install grep
+# TODO: Install sed
+# TODO: Install less
+# TODO: Install touch
+# TODO: Install exiftool 
+# TODO: SysInternals install
+#------------------------------------------------------------------------------
+# > Development
+#------------------------------------------------------------------------------
+# TODO: Install Python3.X
+# TODO: Install asciidoctor
+# TODO: Install Go
+# TODO: Get Windows Terminal Preview
+# TODO: Set up PowerShell aliases
+#------------------------------------------------------------------------------
+# > Forensics 
+#------------------------------------------------------------------------------
+# TODO: Install NirSoft Bucket
+
