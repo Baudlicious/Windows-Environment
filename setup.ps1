6be652dd4dc69ff1a851af44a7fb8be020455af5
Baudlicious <x3830@protonmail.com>
2019-09-10 09:40:33 -0400

    Changes to Scoop

--- setup.ps1
+++ setup.ps1
@@ -1,17 +1,24 @@
 #------------------------------------------------------------------------------
+#     Baudlicious' Script for Setting up a Preferred Windows Environment      #
+#------------------------------------------------------------------------------
+
+$forensics_install = Read-Host -Prompt "> Do you require forensic tools? (Y | N)"
+$extra_install = Read-Host -Prompt "> Do you want the extra utilities? (Y | N)"
+
+#------------------------------------------------------------------------------
 # > Environment
 #------------------------------------------------------------------------------
 $github = "C:\Github"
 $tools = "C:\Tools"
 
 # TODO: Create a C:\Tools folder
+# TODO: Create a C:\Github folder
 # TODO: Install Scoop
 Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
 # TODO: Install aria2
 scoop install aria2
-# TODO: Install FireFox
+# TODO: Install FireFox (this is after Aria2, Scoop will use Aria2 for dl)
 scoop install Firefox
-# TODO: Create a C:\Github folder
 # TODO: Scoop bucket add extras
 scoop bucket add extras
 
@@ -19,32 +26,21 @@ scoop bucket add extras
 # > Utilities
 #------------------------------------------------------------------------------
 # 100% Always utilities
-# TODO: Install 7zip
-# TODO: Install NerdFonts
-# TODO: Install Notepadplusplus
-# TODO: Install NVIM
-### TODO: Setup NVIM with Windows-Environment 
-# TODO: Install Git
+scoop install 7zip NerdFonts Notepadplusplus nvim git openvpn
+# TODO: Setup NVIM with Windows-Environment 
 
 # Bonus Utilities
-# TODO: Install WireShark
-# TODO: Install Wxhexeditor
-# TODO: Install grep
-# TODO: Install sed
-# TODO: Install less
-# TODO: Install touch
-# TODO: Install exiftool 
-# TODO: SysInternals install
+scoop install wireshark wxhexeditor grep sed less touch exiftool sysinternals
+
 #------------------------------------------------------------------------------
 # > Development
 #------------------------------------------------------------------------------
-# TODO: Install Python3.X
-# TODO: Install asciidoctor
-# TODO: Install Go
-# TODO: Get Windows Terminal Preview
+scoop install python3 asciidoctor go 
 # TODO: Set up PowerShell aliases
+
 #------------------------------------------------------------------------------
 # > Forensics 
 #------------------------------------------------------------------------------
-# TODO: Install NirSoft Bucket
+# Adds NirSoft bucket to allow for downloading NirSoft Tools
+scoop bucket add extras
 
